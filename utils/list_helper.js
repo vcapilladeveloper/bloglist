94625e92364c150cbaaf4c59669ff5c2f5ebc117
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-21 19:50:29 +0200

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    const initialValue = 0;
+    const sumWithInitial = likes.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        initialValue,
+    );
+    return sumWithInitial
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
